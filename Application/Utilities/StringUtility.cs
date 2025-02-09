46effa612e0d87fd7a767797739a58a3325c67fa
vlad <arefin.vlad@gmail.com>
2025-02-10 00:43:15 +0300

    Created class for string conversion

---
+++ Application/Utilities/StringUtility.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Utilities;
+
+public static class StringUtility
+{
+    public static string[] GetDataLines(string content)
+    {
+        return content.Split(["],", "], "], StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public static string[] GetValuesFromLine(string line)
+    {
+        var trimmedLine = line.Trim().Trim('[', ']');
+        return trimmedLine.Split(',');
+    }
+
+    public static decimal ConvertFloatToDecimal(string value)
+    {
+        return (decimal)Convert.ToDouble(value.Replace(".", ","));
+    }
+}
